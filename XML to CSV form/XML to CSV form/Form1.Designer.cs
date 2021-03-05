f8eb5ab6ba2077d46447cbc837a6526e54f30916
DylanArts <42747113+DylanArts@users.noreply.github.com>
2021-03-05 09:31:51 +0100

    Application V1.0

---
+++ XML to CSV form/XML to CSV form/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace XML_to_CSV_form
+{
+    partial class XMLtoCSVParser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XMLtoCSVParser));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ParseBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SaveCSVBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(23)))));
+            this.OpenBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenBtn.ForeColor = System.Drawing.Color.White;
+            this.OpenBtn.Location = new System.Drawing.Point(12, 12);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(87, 23);
+            this.OpenBtn.TabIndex = 0;
+            this.OpenBtn.Text = "Open XML File";
+            this.OpenBtn.UseVisualStyleBackColor = false;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(23)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(105, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 13);
+            this.textBox1.TabIndex = 2;
+            // 
+            // ParseBtn
+            // 
+            this.ParseBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(23)))));
+            this.ParseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ParseBtn.ForeColor = System.Drawing.Color.White;
+            this.ParseBtn.Location = new System.Drawing.Point(339, 12);
+            this.ParseBtn.Name = "ParseBtn";
+            this.ParseBtn.Size = new System.Drawing.Size(82, 23);
+            this.ParseBtn.TabIndex = 3;
+            this.ParseBtn.Text = "Parse to CSV";
+            this.ParseBtn.UseVisualStyleBackColor = false;
+            this.ParseBtn.Click += new System.EventHandler(this.ParseBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(19)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(409, 264);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // SaveCSVBtn
+            // 
+            this.SaveCSVBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(23)))));
+            this.SaveCSVBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveCSVBtn.ForeColor = System.Drawing.Color.White;
+            this.SaveCSVBtn.Location = new System.Drawing.Point(174, 316);
+            this.SaveCSVBtn.Name = "SaveCSVBtn";
+            this.SaveCSVBtn.Size = new System.Drawing.Size(87, 23);
+            this.SaveCSVBtn.TabIndex = 5;
+            this.SaveCSVBtn.Text = "Save CSV file";
+            this.SaveCSVBtn.UseVisualStyleBackColor = false;
+            this.SaveCSVBtn.Visible = false;
+            this.SaveCSVBtn.Click += new System.EventHandler(this.SaveCSVBtn_Click);
+            // 
+            // XMLtoCSVParser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(433, 351);
+            this.Controls.Add(this.SaveCSVBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ParseBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OpenBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(449, 390);
+            this.MinimumSize = new System.Drawing.Size(449, 390);
+            this.Name = "XMLtoCSVParser";
+            this.Text = "XML to CSV parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ParseBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button SaveCSVBtn;
+    }
+}
+
---
+++ XML to CSV form/XML to CSV form/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XML_to_CSV_form
+{
+    public partial class XMLtoCSVParser : Form
+    {
+        public XMLtoCSVParser()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenBtn.FlatAppearance.BorderColor = Color.Black;
+            ParseBtn.FlatAppearance.BorderColor = Color.Black;
+            SaveCSVBtn.FlatAppearance.BorderColor = Color.Green;
+        }
+
+        private string xmlFilePath = string.Empty;                              //File path of XML dataset
+        private string xmlName = string.Empty;                                  //File name of XML dataset file
+        private string csvFilePath = string.Empty;                              //File path where CSV file will be downloaded
+        private List<string> csvString = new List<string>();                    //CSV string that will be filled with the parsed data
+
+        private void OpenBtn_Click(object sender, EventArgs e)               
+        {
+            SaveCSVBtn.Visible = false;
+            richTextBox1.Text = "";
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())        //Open File Dialog where you can pick a XML dataset
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "XML Files (*.xml)|*.xml";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)             //If File is picked set parameters
+                {
+                    try
+                    {
+                        //Get the path of specified file
+                        xmlFilePath = openFileDialog.FileName;
+                        xmlName = openFileDialog.SafeFileName;
+                        xmlName = xmlName.Substring(0, xmlName.Length - 4);
+
+                        textBox1.Text = xmlFilePath;
+                    }
+                    catch(Exception exc)
+                    {
+                        richTextBox1.Text += "Check if your file isn't corrupt" + "\r\n";
+                    }
+                }
+            }
+        }
+
+        private void ParseBtn_Click(object sender, EventArgs e)
+        {
+            XDocument xml;
+
+            try
+            {
+                xml = XDocument.Load(xmlFilePath);
+
+                string value = "";
+
+                int count = 0;
+
+                foreach (var node in xml.DescendantNodes())                     //For each node in the XML elements
+                {
+                    if (node is XText && ((XText)node).Value != $"\",\"")       //Check for values and check for the end of a line
+                    {
+                        if (value.Length > 0)
+                        {
+                            value += ";" + ((XText)node).Value;
+                        }
+                        else
+                        {
+                            value += ((XText)node).Value;
+                        }
+                    }
+                    else if (node is XText)                                     //If it is a value and not a end of a line add the value to a line
+                    {
+                        count++;
+                        csvString.Add(value);
+                        value = "";
+                    }
+                }
+
+                richTextBox1.Text += "Parsed XML" + "\r\n";
+                richTextBox1.Text += "Read " + count + " lines!" + "\r\n";
+
+                SaveCSVBtn.Visible = true;                                      //Enable save button for CSV dataset download                        
+            }
+            catch(Exception exc)                                                //Check if XML file isn't corrupt
+            {
+                richTextBox1.Text += "ERROR:" + e + "\r\n";
+                richTextBox1.Text += "Check if your file isn't corrupt" + "\r\n";
+            }
+                  
+
+        }
+
+        private void SaveCSVBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pick a directory to save the CSV file in");
+
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())      //Open Folder Dialog to pick folder where CSV file will be saved
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    csvFilePath = dialog.SelectedPath + "/" + xmlName + ".csv";
+
+                    if (CheckFileExists(csvFilePath))
+                    {
+                        WriteCSVLine(csvString);
+                    }
+                }
+            }
+        }
+        
+        private bool CheckFileExists(string path)
+        {
+            if (File.Exists(path))                                              //Check if file is already present
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to delete the present file?", "", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)                           //If user wants to delete the file
+                {
+                    try
+                    {
+                        File.Delete(path);
+                        richTextBox1.Text += "Deleted existing file" + "\r\n";
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        richTextBox1.Text += "ERROR: Something went wrong while deleting the present file!" + "\r\n";
+                    }
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    richTextBox1.Text += "Canceled CSV save!" + "\r\n";
+                }
+            }
+
+            return false;
+        }
+
+        private void WriteCSVLine(List<string> lines)
+        {
+            richTextBox1.Text += "Writing CSV" + "\r\n";
+
+            int count = 0;
+
+            try
+            {
+                using (StreamWriter file = new StreamWriter(csvFilePath, true))
+                {
+                    foreach (string s in lines)
+                    {
+                        count++;
+                        file.WriteLine(s);                                  //Write CSV file for each CSV data in parameter
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                richTextBox1.Text +="ERROR: Another process is probably using the document provided" + "\r\n";
+                Console.ReadLine();
+            }
+
+            richTextBox1.Text += "Saved the csv file to > " + xmlFilePath + @"\" + xmlName + ".csv";
+        }
+    }
+}
